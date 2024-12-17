5af22b80e039b17c41b29329fa806c64d7ae0f2c
osbm <osmanfbayram@gmail.com>
2024-12-17 11:52:11 +0300

    show tabs and trailing spaces and whatnot

--- config/options.nix
+++ config/options.nix
@@ -3,5 +3,9 @@
         # Enable relative line numbers
         number = true;
         relativenumber = true;
+
+	# show spaces tabs and trailing spaces
+	list = true;
+	listchars = "tab:» ,trail:·,multispace:·,lead: ,nbsp:⎕";
     };
 }
