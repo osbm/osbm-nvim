fdedca413f7f682a6d3f97c61cc5b47afe398765
osbm <osmanfbayram@gmail.com>
2025-01-19 16:24:55 +0300

    add new LSPs

--- plugins/default.nix
+++ plugins/default.nix
@@ -1,3 +1,5 @@
+{ self, lib, pkgs, ... }:
+
 {
   plugins = {
     treesitter = {
@@ -21,6 +23,80 @@
       };
     };
 
+    lsp = {
+      servers = {
+        nixd = {
+          enable = true;
+          settings =
+            let
+              flake = ''(builtins.getFlake "${self}")'';
+            in
+            {
+              nixpkgs.expr = "import ${flake}.inputs.nixpkgs { }";
+              formatting = {
+                command = [ "${lib.getExe pkgs.nixfmt-rfc-style}" ];
+              };
+            };
+        };
+        typos_lsp = {
+          enable = true;
+          extraOptions = {
+            init_options = {
+              diagnosticSeverity = "Hint";
+            };
+          };
+        };
+        pylsp = {
+          enable = true;
+          settings = {
+            configurationSources = ["flake8"];
+            plugins = {
+              pycodestyle = {
+                enabled = true;
+                maxLineLength = 120;
+              };
+              flake8 = {
+                enabled = true;
+                maxLineLength = 120;
+              };
+            };
+          };
 
+        };
+        astro = {
+          enable = true;
+        };
+        arduino_language_server = {
+          enable = true;
+        };
+        bashls= {
+          enable = true;
+        };
+        clangd = {
+          enable = true;
+        };
+        dartls = {
+          enable = true;
+        };
+        lua_ls = {
+          enable = true;
+        };
+        mdx_analyzer = {
+          enable = true;
+        };
+        fish_lsp = {
+          enable = true;
+        };
+        docker_compose_language_service = {
+          enable = true;
+        };
+        dockerls = {
+          enable = true;
+        };
+        marksman = {
+          enable = true;
+        };
+      };
+    };
   };
 }
