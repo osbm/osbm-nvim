5419ff210d3167867862b9e8e72d31aac7eac51c
osbm <osmanfbayram@gmail.com>
2025-03-07 16:40:15 +0300

    formatting

--- plugins/lsp/nix.nix
+++ plugins/lsp/nix.nix
@@ -1,12 +1,10 @@
-{lib, pkgs, self, ...}: {
+{ lib, pkgs, self, ... }: {
   plugins.lsp.servers.nixd = {
     enable = true;
     settings = let flake = ''(builtins.getFlake "${self}")'';
     in {
       nixpkgs.expr = "import ${flake}.inputs.nixpkgs { }";
-      formatting = {
-        command = [ "${lib.getExe pkgs.nixfmt-rfc-style}" ];
-      };
+      formatting.command = [ "${lib.getExe pkgs.nixfmt-rfc-style}" ];
     };
   };
 }
