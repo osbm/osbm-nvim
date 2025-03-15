178c7f94bfd5789b533b4b3313ecb8e5cac0e050
osbm <osmanfbayram@gmail.com>
2025-03-16 00:35:30 +0300

    add keymaps to nvim-cmp

--- plugins/cmp.nix
+++ plugins/cmp.nix
@@ -3,13 +3,21 @@
     autoEnableSources = true;
     settings = {
       sources = [
+        # LSP source
         { name = "nvim_lsp"; }
+        # For path completion
         { name = "path"; }
+        # For buffer completion
         { name = "buffer"; }
+        # For command line completion
         { name = "cmdline"; }
       ];
       mapping = {
+        # <CR> means enter
         "<CR>" = "cmp.mapping.confirm({ select = true })";
+        "<Tab>" = "cmp.mapping.select_next_item()";
+        "<S-Tab>" = "cmp.mapping.select_prev_item()";
+        "<C-e>" = "cmp.mapping.close()";
       };
     };
   };
