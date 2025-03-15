e57b9c080bfe4dd1d8d56c608df681125ca259ec
osbm <osmanfbayram@gmail.com>
2025-03-15 23:27:45 +0300

    add my first keymap

--- plugins/cmp.nix
+++ plugins/cmp.nix
@@ -1,11 +1,16 @@
 {
   plugins.cmp = {
     autoEnableSources = true;
-    settings.sources = [
-      { name = "nvim_lsp"; }
-      { name = "path"; }
-      { name = "buffer"; }
-      { name = "cmdline"; }
-    ];
+    settings = {
+      sources = [
+        { name = "nvim_lsp"; }
+        { name = "path"; }
+        { name = "buffer"; }
+        { name = "cmdline"; }
+      ];
+      mapping = {
+        "<CR>" = "cmp.mapping.confirm({ select = true })";
+      };
+    };
   };
 }
