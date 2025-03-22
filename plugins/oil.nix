8595d33ba5a1520fff57eb42dab159c5f00a2224
osbm <osmanfbayram@gmail.com>
2025-03-23 01:20:23 +0300

    show hidden files

--- plugins/oil.nix
+++ plugins/oil.nix
@@ -3,7 +3,11 @@
   lib,
   ...
 }: {
-  plugins.oil = {};
+  plugins.oil.settings = {
+    view_options = {
+      show_hidden = false;
+    };
+  };
   # add this keymaps only if oil plugin is enabled
   keymaps = lib.mkIf config.plugins.oil.enable [
     {
