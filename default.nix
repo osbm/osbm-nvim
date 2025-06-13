0b1b25d11b822aca6eb4847815d67f6e0174c18b
osbm <osmanfbayram@gmail.com>
2025-06-14 01:40:17 +0300

    add this section

--- default.nix
+++ default.nix
@@ -31,6 +31,23 @@
       ripgrep
     ];
 
+  keymaps = [
+    {
+      mode = "n";
+      key = "<leader>gg";
+      action = "<cmd>LazyGit<CR>";
+      options = {
+        desc = "LazyGit (root dir)";
+      };
+    }
+  ];
+extraPlugins = with pkgs.vimPlugins; [
+    lazygit-nvim
+  ];
+
+  extraConfigLua = ''
+    require("telescope").load_extension("lazygit")
+  '';
     performance = {
       byteCompileLua = {
         enable = true;
