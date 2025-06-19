bb8391d7d9518e85fcd4dd621f81b577ee24e52d
osbm <osmanfbayram@gmail.com>
2025-06-19 03:13:19 +0300

    avante saves the day

---
+++ config/plugins/avante.nix
@@ -0,0 +1,15 @@
+{
+  plugins.avante.settings = {
+    provider = "claude";
+      providers = {
+        claude = {
+          endpoint = "https://api.anthropic.com";
+          extra_request_body = {
+            max_tokens = 4096;
+            temperature = 0;
+          };
+          model = "claude-3-5-sonnet-20240620";
+        };
+      };
+    };
+}
