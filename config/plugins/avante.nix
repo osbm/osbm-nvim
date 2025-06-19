657fba924676c42beefaea224abbdbb405f7e9f8
osbm <osmanfbayram@gmail.com>
2025-06-19 12:37:26 +0300

    this causes problems for some reason

--- config/plugins/avante.nix
+++ config/plugins/avante.nix
@@ -2,17 +2,17 @@
   plugins.avante = {
     lazyLoad.settings.event = ["DeferredUIEnter"];
     settings = {
-      provider = "claude";
-      providers = {
-        claude = {
-          endpoint = "https://api.anthropic.com";
-          extra_request_body = {
-            max_tokens = 4096;
-            temperature = 0;
-          };
-          model = "claude-3-5-sonnet-20240620";
-        };
-      };
+      # provider = "claude";
+      # providers = {
+      #   claude = {
+      #     endpoint = "https://api.anthropic.com";
+      #     extra_request_body = {
+      #       max_tokens = 4096;
+      #       temperature = 0;
+      #     };
+      #     model = "claude-3-5-sonnet-20240620";
+      #   };
+      # };
     };
   };
 }
