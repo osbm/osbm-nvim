a13d094773786bdfb236032ae900bfabaf289f7c
osbm <osbm@osbm.dev>
2025-08-18 17:24:08 +0300

    add blink sources

--- config/plugins/blink-cmp.nix
+++ config/plugins/blink-cmp.nix
@@ -1,6 +1,34 @@
 {
   plugins.blink-cmp = {
     settings = {
+      sources = {
+        default = [
+          "lsp"
+          "path"
+          "luasnip"
+          "buffer"
+          "copilot"
+        ];
+        providers = {
+          copilot = {
+            async = true;
+            module = "blink-copilot";
+            name = "copilot";
+            score_offset = 100;
+            # Optional configurations
+            opts = {
+              max_completions = 3;
+              max_attempts = 4;
+              kind = "Copilot";
+              debounce = 750;
+              auto_refresh = {
+                backward = true;
+                forward = true;
+              };
+            };
+          };
+        };
+      };
     };
   };
 }
