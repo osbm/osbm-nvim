d8efd81cbdfacef5a029b20171f9ce3814b97f96
osbm <osbm@osbm.dev>
2025-08-19 14:51:47 +0300

    set providers

--- config/plugins/avante.nix
+++ config/plugins/avante.nix
@@ -2,8 +2,9 @@
   plugins.avante = {
     lazyLoad.settings.event = ["DeferredUIEnter"];
     settings = {
+      # provider = "ollama";
       # provider = "claude";
-      # providers = {
+      providers = {
       #   claude = {
       #     endpoint = "https://api.anthropic.com";
       #     extra_request_body = {
@@ -12,7 +13,11 @@
       #     };
       #     model = "claude-3-5-sonnet-20240620";
       #   };
-      # };
+        ollama = {
+          endpoint = "http://localhost:11434";
+          model = "qwen2.5-coder:32b";
+        };
+      };
     };
   };
 }
