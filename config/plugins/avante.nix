73e5ab059ffd44d19dfa11ac6bc50a2d554c1dc4
osbm <osbm@osbm.dev>
2025-09-15 16:00:11 +0300

    formatting

--- config/plugins/avante.nix
+++ config/plugins/avante.nix
@@ -5,14 +5,14 @@
       # provider = "ollama";
       # provider = "claude";
       providers = {
-      #   claude = {
-      #     endpoint = "https://api.anthropic.com";
-      #     extra_request_body = {
-      #       max_tokens = 4096;
-      #       temperature = 0;
-      #     };
-      #     model = "claude-3-5-sonnet-20240620";
-      #   };
+        #   claude = {
+        #     endpoint = "https://api.anthropic.com";
+        #     extra_request_body = {
+        #       max_tokens = 4096;
+        #       temperature = 0;
+        #     };
+        #     model = "claude-3-5-sonnet-20240620";
+        #   };
         ollama = {
           endpoint = "http://localhost:11434";
           model = "qwen2.5-coder:32b";
