7a25a807d729da2c5842e095923a9d65ae3b9845
osbm <osbm@osbm.dev>
2025-09-16 13:13:30 +0300

    much better

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   nixConfig = {
     extra-substituters = [
       "https://nix-community.cachix.org"
-      "http://wallfacer.curl-boga.ts.net:7080"
+      "http://wallfacer.curl-boga.ts.net:7080/main"
     ];
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
