ed33e038a72bbec33cadd171f2da897e77799860
osbm <osbm@osbm.dev>
2025-09-17 13:45:11 +0300

    a man can dream

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   nixConfig = {
     extra-substituters = [
       "https://nix-community.cachix.org"
-      "http://wallfacer.curl-boga.ts.net:7080/main"
+      "https://cache.osbm.dev/main"
     ];
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
