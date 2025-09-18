606edf47947fb303e5a5f815ffbb8e3ce74f5583
osbm <osbm@osbm.dev>
2025-09-18 08:51:16 +0300

    Revert "a man can dream"
    This reverts commit ed33e038a72bbec33cadd171f2da897e77799860.

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   nixConfig = {
     extra-substituters = [
       "https://nix-community.cachix.org"
-      "https://cache.osbm.dev/main"
+      "http://wallfacer.curl-boga.ts.net:7080/main"
     ];
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
